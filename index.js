ecb49c01d51fcfc4330781f70dab156a0f03bba9
Hazeyy <126386525+Ryy00@users.noreply.github.com>
2024-03-17 00:03:59 +0800

    index.js

--- index.js
+++ index.js
@@ -204,7 +204,7 @@ app.post('/login', async (req, res) => {
 		}
 });
 const port = process.env.PORT || 3000;
-app.listen(3000, () => {
+app.listen(port, () => {
 		console.log(`
 ───────────────────██
 ──────────────────█─██
