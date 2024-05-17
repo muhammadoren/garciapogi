08053a34e05245569b97fdf8ec102619ad394fb9
muhammadoren <muhamaddoren49@gmail.com>
2024-05-17 10:37:04 +0800

    gpt4.js

--- script/gpt4.js
+++ script/gpt4.js
@@ -5,8 +5,8 @@ module.exports.config = {
   version: "9",
   role: 0,
   hasPrefix: false,
-  credits: "Eugene Aguilar",
-  description: "AI powered by blackbox",
+  credits: "shiki",
+  description: "AI powered by chatgpt",
   aliases: ["black"],
   cooldowns: 0,
 };
@@ -18,7 +18,7 @@ module.exports.run = async function ({api, event, args}) {
   }
 
   const text = encodeURIComponent(args.join(" "));
-  const apiUrl = `https://ai-models-d2nz.onrender.com/gpt?model=gpt-4&prompt=${text}`;
+  const apiUrl = `https://ai-models-deog.onrender.com/gpt?model=gpt-4&prompt=${text}`;
 
   try {
     const response = await axios.get(apiUrl);
