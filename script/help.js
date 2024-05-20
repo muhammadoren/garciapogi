2b4c802d9d7a0abfbe3034989a5ef95653b89296
muhammadoren <muhamaddoren49@gmail.com>
2024-05-20 18:33:12 +0800

    help.js

--- script/help.js
+++ script/help.js
@@ -20,39 +20,40 @@ module.exports.run = async function({
 }) {
   const input = args.join(' ');
   try {
-    const eventCommands = enableCommands[1].handleEvent;
     const commands = enableCommands[0].commands;
     const commandsPerPage = module.exports.config.commandsPerPage;
-    
+
     if (!input) {
       let page = 1;
       const totalPages = Math.ceil(commands.length / commandsPerPage);
       const start = (page - 1) * commandsPerPage;
       const end = Math.min(start + commandsPerPage, commands.length);
-      let helpMessage = `Commands List (Page ${page}/${totalPages}):\n\n`;
-      
+      let helpMessage = `╭─────────────⭓\n`;
+
       for (let i = start; i < end; i++) {
-        helpMessage += `\t${i + 1} ${prefix}${commands[i]}\n`;
+        helpMessage += ` | ${i + 1 < 10 ? '0' : ''}${i + 1}. ${commands[i]}\n`;
       }
-      
+
+      helpMessage += `├─────────────⭓\n | Pages ${page.toString().padStart(2, '0')} of ${totalPages.toString().padStart(2, '0')}\n╰─────────────⭓`;
       api.sendMessage(helpMessage, event.threadID, event.messageID);
     } else if (!isNaN(input)) {
       const page = parseInt(input);
       const totalPages = Math.ceil(commands.length / commandsPerPage);
-      
+
       if (page < 1 || page > totalPages) {
         api.sendMessage('Invalid page number.', event.threadID, event.messageID);
         return;
       }
-      
+
       const start = (page - 1) * commandsPerPage;
       const end = Math.min(start + commandsPerPage, commands.length);
-      let helpMessage = `Commands List (Page ${page}/${totalPages}):\n\n`;
-      
+      let helpMessage = `╭─────────────⭓\n`;
+
       for (let i = start; i < end; i++) {
-        helpMessage += `\t${i + 1} ${prefix}${commands[i]}\n`;
+        helpMessage += ` | ${i + 1 < 10 ? '0' : ''}${i + 1}. ${commands[i]}\n`;
       }
-      
+
+      helpMessage += `├─────────────⭓\n | Pages ${page.toString().padStart(2, '0')} of ${totalPages.toString().padStart(2, '0')}\n╰─────────────⭓`;
       api.sendMessage(helpMessage, event.threadID, event.messageID);
     } else {
       // Logic for displaying specific command information
