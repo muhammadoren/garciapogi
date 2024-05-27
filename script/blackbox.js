d3613636d47fcdf65b6ab83325ef18edcd899bed
muhammadoren <muhamaddoren49@gmail.com>
2024-05-27 17:09:31 +0800

    blackbox.js

--- script/blackbox.js
+++ script/blackbox.js
@@ -1,31 +1,31 @@
 const axios = require('axios');
 
 module.exports.config = {
-	name: "blackbox",
-	version: "9",
-	role: 0,
-	hasPrefix: false,
-	credits: "Eugene Aguilar",
-	description: "AI powered by blackbox",
-	aliases: ["black"],
-	cooldowns: 0,
+  name: "blackbox",
+  version: "9",
+  role: 0,
+  hasPrefix: false,
+  credits: "shiki",
+  description: "AI powered by duckgo",
+  aliases: ["ai"],
+  cooldowns: 0,
 };
 
 module.exports.run = async function ({api, event, args}) {
-	if (!args[0]) {
-		api.sendMessage("Please provide a question.", event.threadID, event.messageID);
-		return;
-	}
+  if (!args[0]) {
+    api.sendMessage("Please provide a question.", event.threadID, event.messageID);
+    return;
+  }
 
-	const query = encodeURIComponent(args.join(" "));
-	const apiUrl = `https://api.easy-api.online/api/blackbox?query=${query}`;
+  const query = encodeURIComponent(args.join(" "));
+  const apiUrl = `https://733eac84-c699-4d7f-a250-3185664c7126-00-1j4jbrhowenda.pike.replit.dev/ai=blackbox=${query}`;
 
-	try {
-		const response = await axios.get(apiUrl);
-		const ans = response.data.response;
-		api.sendMessage(ans, event.threadID, event.messageID);
-	} catch (error) {
-		console.error("Error:", error);
-		api.sendMessage("An error occurred while fetching the response.", event.threadID, event.messageID);
-	}
+  try {
+    const response = await axios.get(apiUrl);
+    const ans = response.data.response;
+    api.sendMessage(ans, event.threadID, event.messageID);
+  } catch (error) {
+    console.error("Error:", error);
+    api.sendMessage("An error occurred while fetching the response.", event.threadID, event.messageID);
+  }
 };
