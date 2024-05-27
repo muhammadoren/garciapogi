dddd4eb30cae764e2d1c741576c1d5ed321e23e0
muhammadoren <muhamaddoren49@gmail.com>
2024-05-27 23:27:20 +0800

    gpt4.js

--- script/gpt4.js
+++ script/gpt4.js
@@ -2,30 +2,34 @@ const axios = require('axios');
 
 module.exports.config = {
   name: "gpt4",
-  version: "9",
-  role: 0,
-  hasPrefix: false,
+  version: "1.0.0",
   credits: "shiki",
-  description: "AI powered by chatgpt",
-  aliases: ["black"],
-  cooldowns: 0,
+  hasPermission: 0,
+  commandCategory: "utility",
+  usage: "[prefix]pi [query]",
+  usePrefix: true,
+  cooldown: 0
 };
 
-module.exports.run = async function ({api, event, args}) {
-  if (!args[0]) {
-    api.sendMessage("Please provide a question.", event.threadID, event.messageID);
-    return;
-  }
-
-  const text = encodeURIComponent(args.join(" "));
-  const apiUrl = `https://ai-models-deog.onrender.com/gpt?model=gpt-4&prompt=${text}`;
-
+module.exports.run = async ({ api, event, args }) => {
   try {
-    const response = await axios.get(apiUrl);
-    const output = response.data.gpt; // Access the "gpt" property in the JSON response
-    api.sendMessage(output, event.threadID, event.messageID);
+    const query = args.join(" ");
+    if (query) {
+      const processingMessage = await api.sendMessage(`Searching🔍. Please wait a moment...`, event.threadID);
+
+      const response = await axios.get(`https://gpt4-l59t.onrender.com/gpt4=${encodeURIComponent(query)}`);
+
+      if (response.data && response.data.response) {
+        await api.sendMessage({ body: response.data.response.trim() }, event.threadID, event.messageID);
+        console.log(`Sent GPT-4's response to the user`);
+      } else {
+        throw new Error(`Invalid or missing response from GPT-4 API`);
+      }
+
+      await api.unsendMessage(processingMessage.messageID);
+    }
   } catch (error) {
-    console.error("Error:", error);
-    api.sendMessage("An error occurred while fetching the response.", event.threadID, event.messageID);
+    console.error(`❌ | Failed to get GPT-4's response: ${error.message}`);
+    api.sendMessage(`❌ | An error occurred. You can try typing your query again or resending it. There might be an issue with the server that's causing the problem, and it might resolve on retrying.`, event.threadID);
   }
 };
