5fc1621c8cdb2aa5f65ed01e485f705987619896
muhammadoren <muhamaddoren49@gmail.com>
2024-06-08 10:53:09 +0800

    Claude.js

--- script/Claude.js
+++ script/Claude.js
@@ -6,8 +6,8 @@ module.exports.config = {
   role: 0,
   hasPrefix: false,
   credits: "shiki",
-  description: "AI powered by duckgo",
-  aliases: ["claude"],
+  description: "AI powered by claude",
+  aliases: ["ai"],
   cooldowns: 0,
 };
 
@@ -17,13 +17,15 @@ module.exports.run = async function ({api, event, args}) {
     return;
   }
 
-  const query = encodeURIComponent(args.join(" "));
-  const apiUrl = `https://g4f-eqak.onrender.com/ai=claude-3-haiku=${query}`;
+  const text = encodeURIComponent(args.join(" "));
+  const apiUrl = `https://claude-n17l.onrender.com/claude?question=${text}`;
 
   try {
     const response = await axios.get(apiUrl);
-    const ans = response.data.response;
-    api.sendMessage(ans, event.threadID, event.messageID);
+    const content = response.data; // Directly access the JSON response
+    const answer = content.response; // Access the "response" property in the JSON
+
+    api.sendMessage(answer, event.threadID, event.messageID);
   } catch (error) {
     console.error("Error:", error);
     api.sendMessage("An error occurred while fetching the response.", event.threadID, event.messageID);
